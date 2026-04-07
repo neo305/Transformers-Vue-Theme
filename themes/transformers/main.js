93b5a4df24566b45dbead75f08a0f3a4b331f811
neo305 <laz305@gmail.com>
2026-04-07 17:14:19 -0400

    Update BGM Code
    no need for sfx folder now

--- themes/transformers/main.js
+++ themes/transformers/main.js
@@ -23,6 +23,14 @@
   if (typeof startBgmIfEnabled === 'function') {
     startBgmIfEnabled();
   }
+    if (typeof jsmaf.bgm === 'undefined') {
+    jsmaf.bgm = new jsmaf.AudioClip();
+    jsmaf.bgm.volume = 0.5;
+    jsmaf.bgm.open("file://../download0/themes/transformers/img/bgm.wav");
+  }
+  var bgm = jsmaf.bgm;
+
+  bgm.play(true);
   var background = new Image({
     url: 'file:///../download0/themes/transformers/img/Fondo.jpg',
     x: 0,
